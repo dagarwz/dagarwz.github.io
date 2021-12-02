7669543740b322af93e7f906f2b9cb723fb4a0d4
dagarwz <divaa@andrew.cmu.edu>
2021-12-02 18:56:18 -0500

    making nav links work, kinda

--- homework_8/script.js
+++ homework_8/script.js
@@ -18,3 +18,116 @@ let scene = new ScrollMagic.Scene({
   .setTween(timeline)
   .setPin("section")
   .addTo(controller)
+
+// function scrolltoSection(id) {
+//   var page = document.getElementById(id)
+//   console.log(page)
+//   page.scrollIntoView()
+// }
+
+// Init controller
+var controller2 = new ScrollMagic.Controller({
+  globalSceneOptions: {
+    duration: $("section").height(),
+    triggerHook: 0.025,
+    reverse: true,
+  },
+})
+
+var scenes = {
+  intro: {
+    intro: "intro-anchor",
+  },
+  scene2: {
+    contentSection: "anchor1",
+  },
+  scene3: {
+    itinSection: "anchor2",
+  },
+}
+
+for (var key in scenes) {
+  // skip loop if the property is from prototype
+  if (!scenes.hasOwnProperty(key)) continue
+
+  var obj = scenes[key]
+
+  for (var prop in obj) {
+    // skip loop if the property is from prototype
+    if (!obj.hasOwnProperty(prop)) continue
+
+    new ScrollMagic.Scene({ triggerElement: "#" + prop })
+      .setClassToggle("#" + obj[prop], "active")
+      .addTo(controller2)
+  }
+}
+
+controller2.scrollTo(function (target) {
+  console.log("target", target)
+  // TweenMax.to(window, 0.5, {
+  //   scrollTo: {
+  //     y: 740,
+  //     autoKill: true, // Allow scroll position to change outside itself
+  //   },
+  //   ease: Cubic.easeInOut,
+  // })
+  scrollOptions = {
+    top: target,
+    behavior: "smooth",
+  }
+
+  window.scrollTo(scrollOptions)
+})
+
+var anchor_nav = document.querySelector(".anchor-nav")
+
+anchor_nav.addEventListener("click", function (e) {
+  var target = e.target,
+    id = target.getAttribute("href")
+  console.log(id)
+  let top = 0
+  if (id !== null) {
+    if (id.length > 0) {
+      e.preventDefault()
+      if (id == "#contentSection") top = 2000
+      if (id == "#itinSection") top = 20000
+      scrollOptions = {
+        top: top,
+        behavior: "smooth",
+      }
+
+      window.scrollTo(scrollOptions)
+      if (window.history && window.history.pushState) {
+        history.pushState("", document.title, id)
+      }
+    }
+  }
+})
+
+// document.getElementById("anchor2").addEventListener("click", function (e) {
+//   console.log("yo")
+//   document.getElementById("anchor2").click()
+// })
+
+function runAgain(e) {
+  var target = e.target,
+    id = target.getAttribute("href")
+  console.log(id)
+  let top = 0
+  if (id !== null) {
+    if (id.length > 0) {
+      e.preventDefault()
+      if (id == "#contentSection") top = 2000
+      if (id == "#itinSection") top = 20000
+      scrollOptions = {
+        top: top,
+        behavior: "smooth",
+      }
+
+      window.scrollTo(scrollOptions)
+      if (window.history && window.history.pushState) {
+        history.pushState("", document.title, id)
+      }
+    }
+  }
+}
